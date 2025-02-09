33e9974490af57442f69535abe7742dfc5cbba03
Can <canngo215@gmail.com>
2025-02-09 14:18:05 -0500

    index html

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const { createServer } = require('node:http');
-const { exec } = require('child_process')
+const { exec } = require('node:child_process')
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  console.log(process.platform);
+  exec('start .', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    });
 });
 
 server.listen(port, hostname, () => {
